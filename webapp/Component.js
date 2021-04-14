fef9d28ec25aa9cbb8621b5f777b27446f922607
Miteshpawar <mitesh.pawar@invenio-solutions.com>
2021-04-14 13:43:10 +0530

    First Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ListReportMitesh.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ListReportMitesh.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
